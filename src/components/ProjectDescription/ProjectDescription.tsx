388944b5f52b6a27ee8d3873ba861bc1ac77c705
Tymoteusz Lossy <tymek.lossy@gmail.com>
2022-11-30 19:25:04 +0100

    Project 2 has been added.

--- src/components/ProjectDescription/ProjectDescription.tsx
+++ src/components/ProjectDescription/ProjectDescription.tsx
@@ -1,18 +1,24 @@
 import React, {useContext} from "react";
 import {ProjectDescImg} from "../ProjectDescImg/ProjectDescImg";
 import {ProjectDescText} from "../ProjectDescText/ProjectDescText";
-import {sobItem} from '../../data/projectDescriptions';
+import {asianFood, sobItem} from '../../data/projectDescriptions';
+import {ThemeContext} from "../../context/themeContext";
 
 import './ProjectDescription.css';
-import {ThemeContext} from "../../context/themeContext";
 
 export const ProjectDescription = () => {
     const {darkMode} = useContext(ThemeContext);
 
     return (
-        <div className="project-desc" style={{backgroundColor: darkMode ? 'var(--color-dark-blue)' : 'var(--color-light-beige)'}}>
-            <ProjectDescImg imgTitle={sobItem.imgTitle} images={sobItem.images} link={sobItem.link}/>
-            <ProjectDescText title={sobItem.title} description={sobItem.description}/>
-        </div>
+        <>
+            <div className="project-desc" style={{backgroundColor: darkMode ? 'var(--color-dark-blue)' : 'var(--color-light-beige)'}}>
+                <ProjectDescImg imgTitle={sobItem.imgTitle} images={sobItem.images} link={sobItem.link}/>
+                <ProjectDescText title={sobItem.title} description={sobItem.description}/>
+            </div>
+            <div className="project-desc" style={{backgroundColor: darkMode ? 'var(--color-dark-blue)' : 'var(--color-light-beige)'}}>
+                <ProjectDescText title={asianFood.title} description={asianFood.description}/>
+                <ProjectDescImg imgTitle={asianFood.imgTitle} images={asianFood.images} link={asianFood.link}/>
+            </div>
+        </>
     );
 }
