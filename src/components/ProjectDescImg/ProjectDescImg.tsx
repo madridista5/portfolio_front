f8eaf7e414b68a4281fcaee94b0af22bf3ac84af
Tymoteusz Lossy <tymek.lossy@gmail.com>
2022-11-30 16:24:52 +0100

    Component ProjectDescImg has been prepared for desktop.

--- src/components/ProjectDescImg/ProjectDescImg.tsx
+++ src/components/ProjectDescImg/ProjectDescImg.tsx
@@ -1,5 +1,36 @@
-import React from "react";
+import React, {useState} from "react";
 
-export const ProjectDescImg = () => (
-    <div className="project-desc__img">Img</div>
-)
+import '../ProjectDescription/ProjectDescription.css';
+
+interface Props {
+    title: string,
+    img: string[],
+}
+
+export const ProjectDescImg = ({title, img}: Props) => {
+    const [index, setIndex] = useState<number>(0);
+
+    const handleArrow = (direction: 'L' | 'R') => {
+        if(direction === 'L') {
+            if(index === 0) return;
+            setIndex(index => index - 1);
+        } else if(direction === 'R') {
+            if(index === img.length-1) return;
+            setIndex(index => index + 1);
+        }
+    }
+
+    return (
+        <div className="project-desc__img">
+            <h2 className="project-desc__img-h2">{title}</h2>
+            <div className="project-desc__img-wrapper">
+                <div className="arrow arr-left" onClick={() => handleArrow('L')}>←</div>
+                <div className="arrow arr-right" onClick={() => handleArrow('R')}>→</div>
+                <div className="p-card bg"/>
+                <div className="p-card">
+                    <img src={img[index]} alt="p-img"/>
+                </div>
+            </div>
+        </div>
+    )
+}
--- src/components/ProjectDescription/ProjectDescription.tsx
+++ src/components/ProjectDescription/ProjectDescription.tsx
@@ -1,13 +1,17 @@
 import React from "react";
 import {ProjectDescImg} from "../ProjectDescImg/ProjectDescImg";
 import {ProjectDescText} from "../ProjectDescText/ProjectDescText";
+import imgSobitem1 from '../../assets/sobitem_1.png';
+import imgSobitem2 from '../../assets/sobitem_2.png';
+import imgSobitem3 from '../../assets/sobitem_3.png';
+import imgSobitem4 from '../../assets/sobitem_4.png';
 
 import './ProjectDescription.css';
 
 export const ProjectDescription = () => {
     return (
         <div className="project-desc">
-            <ProjectDescImg/>
+            <ProjectDescImg title="SoBitem" img={[imgSobitem1, imgSobitem2, imgSobitem3, imgSobitem4]}/>
             <ProjectDescText/>
         </div>
     );
